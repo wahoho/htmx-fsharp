41c25dcc43779412fd9e4518395b01f420f86473
Jason Yap <wahoho.yap@gmail.com>
2024-09-22 01:07:39 +0900

    init

---
+++ HelloWorld/Program.fs
@@ -0,0 +1,146 @@
+module HelloWorld.App
+
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Giraffe.ViewEngine.Htmx
+
+// ---------------------------------
+// Models
+// ---------------------------------
+
+type Message =
+    {
+        Text : string
+    }
+
+// ---------------------------------
+// Views
+// ---------------------------------
+
+module Views =
+    open Giraffe.ViewEngine
+
+    let layout (content: XmlNode list) =
+        html [] [
+            head [] [
+                title []  [ encodedText "HelloWorld" ]
+                link [ _rel  "stylesheet"
+                       _type "text/css"
+                       _href "/main.css" ]
+                script [ _src "https://unpkg.com/htmx.org@2.0.2" ] []
+            ]
+            body [] content
+        ]
+
+    let index (model : Message) =
+        [
+            p [] [ encodedText model.Text ]
+            div [ _id "replace" ] []
+            div [] [
+                button [
+                    _hxGet "/clicked";
+                    _hxTarget "#replace"
+                    _hxSwap "outerHTML"
+                ] [ str "Click Me"]
+            ]
+        ] |> layout
+        
+    let clickComponent (model: Message) =
+        div [] [
+            span [] [
+                str model.Text    
+            ]
+        ] 
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let indexHandler (name : string) =
+    let greetings = sprintf "Hello %s, from Giraffe!" name
+    let model     = { Text = greetings }
+    let view      = Views.index model
+    htmlView view
+    
+let clickHandler =
+    let message   = "Hello, from HTMX"
+    let model     = { Text = message }
+    let view      = Views.clickComponent model
+    htmlView view
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route "/" >=> indexHandler "world"
+                route "/clicked" >=> clickHandler 
+                routef "/hello/%s" indexHandler
+            ]
+        setStatusCode 404 >=> text "Not Found" ]
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+// ---------------------------------
+// Config and Main
+// ---------------------------------
+
+let configureCors (builder : CorsPolicyBuilder) =
+    builder
+        .WithOrigins(
+            "http://localhost:5000",
+            "https://localhost:5001")
+       .AllowAnyMethod()
+       .AllowAnyHeader()
+       |> ignore
+
+let configureApp (app : IApplicationBuilder) =
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+    (match env.IsDevelopment() with
+    | true  ->
+        app.UseDeveloperExceptionPage()
+    | false ->
+        app .UseGiraffeErrorHandler(errorHandler)
+            .UseHttpsRedirection())
+        .UseCors(configureCors)
+        .UseStaticFiles()
+        .UseGiraffe(webApp)
+
+let configureServices (services : IServiceCollection) =
+    services.AddCors()    |> ignore
+    services.AddGiraffe() |> ignore
+
+let configureLogging (builder : ILoggingBuilder) =
+    builder.AddConsole()
+           .AddDebug() |> ignore
+
+[<EntryPoint>]
+let main args =
+    let contentRoot = Directory.GetCurrentDirectory()
+    let webRoot     = Path.Combine(contentRoot, "WebRoot")
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseContentRoot(contentRoot)
+                    .UseWebRoot(webRoot)
+                    .Configure(Action<IApplicationBuilder> configureApp)
+                    .ConfigureServices(configureServices)
+                    .ConfigureLogging(configureLogging)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
